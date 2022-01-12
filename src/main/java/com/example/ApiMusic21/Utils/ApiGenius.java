cbd287ccf0fd8b9041906aa5950e7a0fdf2c2622
Dorian GAURON <dorian.gauron@etu.univ-nantes.fr>
2022-01-12 17:46:36 +0100

    Request Rap genius

---
+++ src/main/java/com/example/ApiMusic21/Utils/ApiGenius.java
@@ -0,0 +1,10 @@
+package com.example.ApiMusic21.Utils;
+
+public class ApiGenius {
+
+    private String CLIENT_ID = "NaUPVazqBFcvXZcSmjxdRCWv-iStWlbk8DTKOgbRyTod7eM71YqZ6S-mhz5V4K53";
+    private String KEY = "1wwUqS4q6k87M9zWlKwgGg-OBvLebIGSssri5fpFZOb4X7koJOtWwiQMFLvklEo4C1-afn8I6HcPzkluVzRppg";
+    private String BEARER = "VaWEhs6AwPDPlwtRrdf7b6pua2y-_K34JpDoDjYHbOwHhfikMM0Q2a4IcZopEY0J";
+
+    
+}
