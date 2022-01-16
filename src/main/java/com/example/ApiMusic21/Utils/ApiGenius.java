5d1e95e453e19d94c16088ab411d7833ca558790
La Prise <gaurondorian@gmail.com>
2022-01-16 21:28:17 +0100

    update gitignore

--- src/main/java/com/example/ApiMusic21/Utils/ApiGenius.java
+++ src/main/java/com/example/ApiMusic21/Utils/ApiGenius.java
@@ -6,5 +6,5 @@ public class ApiGenius {
     private String KEY = "1wwUqS4q6k87M9zWlKwgGg-OBvLebIGSssri5fpFZOb4X7koJOtWwiQMFLvklEo4C1-afn8I6HcPzkluVzRppg";
     private String BEARER = "VaWEhs6AwPDPlwtRrdf7b6pua2y-_K34JpDoDjYHbOwHhfikMM0Q2a4IcZopEY0J";
 
-    
+
 }
