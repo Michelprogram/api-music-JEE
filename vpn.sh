ac3e054f609677fb8b02998d4bcf83c1ea8784cb
La Prise <gaurondorian@gmail.com>
2022-01-19 13:56:13 +0100

    update vpn

--- vpn.sh
+++ vpn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get -qq update && apt-get install -qqy --no-install-recommends openconnect maven wget
-openconnect nomade.etu.univ-nantes.fr --juniper --user=berdjugin-jf -b < mdp.txt
+openconnect nomade.etu.univ-nantes.fr E21A432S --juniper --user=berdjugin-jf -b < mdp.txt
